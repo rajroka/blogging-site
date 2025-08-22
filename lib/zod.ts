652955ba99facff1684350f7e0f8a4d87a460952
Raj Roka <rokaraaz@gmail.com>
2025-08-22 03:23:27 -0500

    push

--- lib/zod.ts
+++ lib/zod.ts
@@ -1,11 +1,12 @@
-import { object, string } from "zod"
- 
+import { email, object, string } from "zod"
+
 export const signInSchema = object({
-  email: string({ required_error: "Email is required" })
-    .min(1, "Email is required")
-    .email("Invalid email"),
-  password: string({ required_error: "Password is required" })
-    .min(1, "Password is required")
-    .min(8, "Password must be more than 8 characters")
-    .max(32, "Password must be less than 32 characters"),
+  email: 
+   email({ message: "Invalid email" })
+    .min(1, { message: "Email is required" })
+   ,
+  password: string()
+    .min(1, { message: "Password is required" })
+    .min(8, { message: "Password must be more than 8 characters" })
+    .max(32, { message: "Password must be less than 32 characters" }),
 })
